afaefdbc18f7fb5255484627ebe577d8bc4c9c44
Shehab <11789402+shehabgamin@users.noreply.github.com>
2025-08-28 19:18:57 -0700

    Save

--- crates/core/src/kernel/snapshot/serde.rs
+++ crates/core/src/kernel/snapshot/serde.rs
@@ -171,12 +171,12 @@ impl<'de> Visitor<'de> for SnapshotVisitor {
             .transpose()?
             .flatten();
 
-        let listed_log_files = ListedLogFiles {
+        let listed_log_files = ListedLogFiles::try_new(
             ascending_commit_files,
             ascending_compaction_files,
             checkpoint_parts,
             latest_crc_file,
-        };
+        )?;
 
         let log_root = if !table_url.path().ends_with("/") {
             let mut aux = table_url.clone();
